7707ce8d57009bad2f4e3403cc718559a6825128
fsun7 <fsun7@dxc.com>
2021-04-08 16:21:08 +0800

    配置 axios

--- src/main.js
+++ src/main.js
@@ -6,8 +6,14 @@ import './plugins/element.js'
 import '@/assets/css/global.css'
 // 导入字体图标
 import '@/assets/fonts/iconfont.css'
+// 导入 axios 依赖
+import axios from 'axios'
 
 Vue.config.productionTip = false
+// 配置 this.$http 指向 axios
+Vue.prototype.$http = axios
+// 配置 axios 请求根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 
 new Vue({
   router,
