31ae5a6f4bc28e1f32db6db34dd7b7a23cc0bd1b
fsun7 <fsun7@dxc.com>
2021-04-08 18:19:11 +0800

    完成退出功能

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,23 @@
 <template>
   <div>
     home组件
+    <el-button type="info" @click="logout">退出</el-button>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {}
+  },
+  methods: {
+    logout() {
+      // 清理 token 返回登录页面
+      window.sessionStorage.clear()
+      this.$router.push("/login")
+    }
+  }
+}
 </script>
 <style lang="less" scoped>
 </style>
