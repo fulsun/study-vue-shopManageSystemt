5f9789ef3bcbc06bfb462ccf03a5220e5d4e019f
fl6145 <fl_6145@163.com>
2021-04-12 16:51:20 +0800

    完成搜索功能

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -11,12 +11,12 @@
       <!-- 搜索与添加区域一行有 24个 span-->
       <el-row :gutter="20">
         <el-col :span="6">
-          <el-input placeholder="请输入内容">
-            <el-button slot="append" icon="el-icon-search"></el-button>
+          <el-input placeholder="请输入内容" v-model="queryInfo.query" clearable @clear="clearSearch">
+            <el-button slot="append" icon="el-icon-search" @click="getUserList"></el-button>
           </el-input>
         </el-col>
         <el-col :span="4">
-          <el-button type="primary"> 添加用户</el-button>
+          <el-button type="primary" @click="dialogVisible = true"> 添加用户</el-button>
         </el-col>
       </el-row>
       <el-table :data="userList" stripe border>
@@ -55,6 +55,30 @@
         :total="total">
       </el-pagination>
     </el-card>
+    <el-dialog
+      title="添加用户"
+      :visible.sync="dialogVisible"
+      width="50%"
+      :before-close="handleClose">
+      <el-form :model="addForm" :rules="addFormRules" ref="addFormRef" label-width="70px">
+        <el-form-item label="姓名" prop="username">
+          <el-input v-model="addForm.username"></el-input>
+        </el-form-item>
+        <el-form-item label="手机号" prop="mobile">
+          <el-input v-model="addForm.mobile"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="addForm.email"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="addForm.password"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+    <el-button @click="dialogVisible = false">取 消</el-button>
+    <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+  </span>
+    </el-dialog>
   </div>
 </template>
 <script>
@@ -66,7 +90,50 @@ export default {
         pagenum: 1,
         pagesize: 2
       },
-      total: 0
+      total: 0,
+      dialogVisible: false,
+      // 用户添加的表单数据
+      addForm: {
+        username: ''
+      },
+      addFormRules: {
+        username: [
+          {
+            required: true,
+            message: '请填写用户名',
+            trigger: 'blur'
+          },
+          {
+            min: 3,
+            max: 10,
+            message: '长度在 3 到 10 个字符',
+            trigger: 'blur'
+          }
+        ],
+        email: [{
+          required: true,
+          message: '请填写邮箱',
+          trigger: 'blur'
+        }],
+        mobile: [{
+          required: true,
+          message: '请填写手机号',
+          trigger: 'blur'
+        }],
+        password: [
+          {
+            required: true,
+            message: '请填写密码',
+            trigger: 'blur'
+          },
+          {
+            min: 6,
+            max: 12,
+            message: '长度在 6 到 12个字符',
+            trigger: 'blur'
+          }
+        ]
+      }
     };
   },
   created() {
@@ -92,12 +159,24 @@ export default {
     async userStatuChange(userInfo) {
       // 使用 ``用于字符串的拼接
       const { data: res } = await this.$http.put(`/users/${userInfo.id}/state/${userInfo.mg_state}`);
-      console.log(res);
+      // console.log(res);
       if (res.meta.status !== 200) {
         userInfo.mg_state = !userInfo.mg_state;
         return this.$message.error('更新状态失败');
       }
       this.$message.success('更新状态成功');
+    },
+    clearSearch() {
+      this.getUserList();
+    },
+    handleClose(done) {
+      done();
+      // this.$confirm('确认关闭？')
+      //   .then(_ => {
+      //     done();
+      //   })
+      //   .catch(_ => {
+      //   });
     }
   }
 }
