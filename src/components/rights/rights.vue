42b725de1085961f5bd3fcc92bfec204872c9608
fl6145 <fl_6145@163.com>
2021-04-12 19:38:39 +0800

    完成权限列表展示，使用 tag 标签

--- src/components/rights/rights.vue
+++ src/components/rights/rights.vue
@@ -1,3 +1,43 @@
 <template>
-  <div>权限列表组件</div>
+  <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>权限列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card>
+      <el-table :data="roleList" stripe border>
+        <el-table-column type="index" label="#"></el-table-column>
+        <el-table-column prop="authName" label="权限名称"></el-table-column>
+        <el-table-column prop="path" label="路径"></el-table-column>
+        <el-table-column label="权限等级">
+          <template v-slot="slot">
+            <el-tag v-show='slot.row.level === "0"'>一级</el-tag>
+            <el-tag type="success" v-show='slot.row.level === "1"'>二级</el-tag>
+            <el-tag type="warning" v-show='slot.row.level === "2"'>三级</el-tag>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
+  </div>
 </template>
+
+<script>
+export default {
+  created() {
+    this.getRoleList();
+  },
+  data() {
+    return {
+      roleList: []
+    };
+  },
+  methods: {
+    async getRoleList() {
+      const { data: res } = await this.$http.get('rights/list');
+      if (res.meta.status !== 200) return this.$message.error('权限列表获取失败');
+      this.roleList = res.data;
+    }
+  }
+};
+</script>
