439b4f6ce85f0c2fae3e6e997d7a876c86ff3189
fl6145 <fl_6145@163.com>
2021-04-13 14:54:07 +0800

    设置最小宽度，保证缩放不变形， 完成权限列表的展示

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -15,7 +15,35 @@
       <!-- 角色列表 -->
       <el-table :data="rolesList" stripe border>
         <!-- 展开列 -->
-        <el-table-column type="expand"></el-table-column>
+        <el-table-column type="expand">
+          <template v-slot="slot">
+            <el-row v-for="(item1,index1) in slot.row.children" :key="item1.id"
+                    :class="[{bdtop:index1=='0'},'bdbuttom','vcenter']">
+              <!-- 渲染一级权限 -->
+              <el-col :span="5">
+                <el-tag>{{ item1.authName}}</el-tag>
+                <i class="el-icon-caret-right"></i>
+              </el-col>
+              <!--渲染二级与三级权限-->
+              <el-col :span="19">
+                <el-row v-for="(item2,index2) in item1.children" :key="item2.id"
+                        :class="[{bdtop:index2!='0'},'vcenter']">
+                  <el-col :span="6">
+                    <el-tag type="success"> {{item2.authName}}</el-tag>
+                    <i class="el-icon-caret-right"></i>
+                  </el-col>
+                  <el-col :span="18">
+                    <el-tag v-for="item3 in item2.children" :key="item3.id" type="warning">{{item3.authName}}</el-tag>
+                  </el-col>
+                </el-row>
+              </el-col>
+              <br />
+            </el-row>
+            <!--<pre>-->
+            <!--  {{slot.row}}-->
+            <!--</pre>-->
+          </template>
+        </el-table-column>
         <!-- 索引列 -->
         <el-table-column type="index" label="#"></el-table-column>
         <el-table-column prop="roleName" label="角色名称"></el-table-column>
@@ -24,7 +52,8 @@
           <template v-slot="slot">
             <el-button type="primary" icon="el-icon-edit" size="mini" @click="showEditDialog(slot.row.id)">编辑
             </el-button>
-            <el-button type="danger" icon="el-icon-delete" size="mini" @click="removeRoleById(slot.row.id)">删除</el-button>
+            <el-button type="danger" icon="el-icon-delete" size="mini" @click="removeRoleById(slot.row.id)">删除
+            </el-button>
             <el-button type="warning" icon="el-icon-setting" size="mini">分配权限</el-button>
           </template>
         </el-table-column>
@@ -208,3 +237,22 @@ export default {
   }
 };
 </script>
+
+<style lang="less">
+  .el-tag {
+    margin: 7px;
+  }
+
+  .bdtop {
+    border-top: 1px solid #eeeeee;
+  }
+
+  .bdbuttom {
+    border-bottom: 1px solid #eeeeee;
+  }
+
+  .vcenter {
+    display: flex;
+    align-items: center;
+  }
+</style>
