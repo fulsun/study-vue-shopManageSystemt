adbbffb64770409a3fcb88f0c54b460f028312ae
fl6145 <fl_6145@163.com>
2021-04-14 09:48:18 +0800

    完成分类数据的展示和分页效果

--- src/components/goods/Categories.vue
+++ src/components/goods/Categories.vue
@@ -1,11 +1,116 @@
 <template>
-  <div>商品分类模块</div>
+  <div>
+    <!--面包屑导航区域-->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/welcome' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品分类</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!--卡片区域-->
+    <el-card>
+      <el-row>
+        <el-col>
+          <el-button type="primary">添加分类</el-button>
+        </el-col>
+      </el-row>
+      <tree-table border :class="['treetable']"
+                  :data="cateList.result"
+                  :columns="columns"
+                  :show-index="true"
+                  index-text="#"
+                  :show-row-hover='false'
+                  :selection-type="false"
+                  :expand-type="false">
+        <!-- 是否有效 -->
+        <template v-slot:isOk="scope">
+          <i class="el-icon-success" v-show="scope.row.cat_deleted" style="color: lightgreen"></i>
+          <i class="el-icon-error" v-show="!scope.row.cat_deleted" style="color: orangered"></i>
+        </template>
+        <!-- 排序 -->
+        <template slot="order" slot-scope="scope">
+          <el-tag size="mini" v-show="scope.row.cat_level === 0">一级</el-tag>
+          <el-tag size="mini" type="success" v-show="scope.row.cat_level === 1">二级</el-tag>
+          <el-tag size="mini" type="warning" v-show="scope.row.cat_level === 2">三级</el-tag>
+        </template>
+        <!-- 操作 -->
+        <template slot="opt" slot-scope="scope">
+          <el-button type="primary" icon="el-icon-edit" size="mini" @click="showEditCateDialog(scope.row.cat_id)">编辑
+          </el-button>
+          <el-button type="danger" icon="el-icon-delete" size="mini" @click="removeCate(scope.row.cat_id)">删除
+          </el-button>
+        </template>
+      </tree-table>
+      <!-- 分页区域-->
+      <el-pagination
+        background
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[5, 10, 20, 100]"
+        :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total">
+      </el-pagination>
+    </el-card>
+  </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      cateList: []
+      total: 0,
+      cateList: [],
+      addCateDialogVisible: false,
+      // 添加分类对象
+      addCateForm: {
+        // 将要添加分类名称
+        cat_name: '',
+        // 分类父级id
+        cat_pid: 0,
+        // 分类等级：`0`表示一级分类；`1`表示二级分类；`2`表示三级分类
+        cat_level: 0
+      },
+      // 添加分类表单的验证规则
+      addCateFormRules: {
+        cat_name: [
+          {
+            required: true,
+            message: '请输入分类名称',
+            trigger: 'blur'
+          }
+        ]
+      },
+      // 父级分类列表
+      parentCateList: [],
+      queryInfo: {
+        type: [1, 2, 3],
+        pagenum: 1,
+        pagesize: 5
+      },
+      columns: [
+        {
+          label: '分类名称',
+          prop: 'cat_name'
+        },
+        {
+          label: '是否有效',
+          // 当前列 自定义模板
+          type: 'template',
+          template: 'isOk'
+        },
+        {
+          label: '排序',
+          // 当前列 自定义模板
+          type: 'template',
+          template: 'order'
+        },
+        {
+          label: '操作',
+          // 当前列 自定义模板
+          type: 'template',
+          template: 'opt'
+        }
+      ]
     };
   },
   created() {
@@ -14,13 +119,30 @@ export default {
   methods: {
     //   获取所有的商品分类列表
     async getCateList() {
-      const { data: res } = await this.$http.get('categories');
+      const { data: res } = await this.$http.get('categories', {
+        params: this.queryInfo
+      });
       if (res.meta.status !== 200) {
         return this.$message.error('获取商品数据列表失败！');
       }
       this.cateList = res.data;
-      console.log(res.data);
+      this.total = res.data.total;
+    },
+    // 监听页面展示数据
+    handleSizeChange(newSize) {
+      this.queryInfo.pagesize = newSize;
+      this.getCateList();
+    },
+    // 监听翻页动作
+    handleCurrentChange(newPage) {
+      this.queryInfo.pagenum = newPage;
+      this.getCateList();
     }
   }
 };
 </script>
+<style lang="less">
+  .treetable {
+    margin-top: 15px;;
+  }
+</style>
