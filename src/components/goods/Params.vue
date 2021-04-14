3b799c583ffc3ce09d276bfe2bd480cd905b7da2
fl6145 <fl_6145@163.com>
2021-04-14 15:48:45 +0800

    完成参数的添加功能

--- src/components/goods/Params.vue
+++ src/components/goods/Params.vue
@@ -29,7 +29,8 @@
       <el-tabs v-model="activeTabsName" @tab-click="handleTabsClick">
         <!-- 添加动态参数面板 -->
         <el-tab-pane label="动态参数" name="many">
-          <el-button type="primary" size="medium" :disabled="isBtnDisable">添加参数</el-button>
+          <el-button type="primary" size="medium" :disabled="isBtnDisable" @click="addDialogVisible = true">添加参数
+          </el-button>
           <el-table
             :data="manyTableData" border stripe
             style="width: 100%">
@@ -61,7 +62,9 @@
         </el-tab-pane>
         <!-- 添加静态属性面板 -->
         <el-tab-pane label="静态属性" name="only">
-          <el-button type="primary" size="medium" :disabled="isBtnDisable">添加属性</el-button>
+          <el-button type="primary" size="medium" :disabled="isBtnDisable" @click="addDialogVisible = true"
+          >添加属性
+          </el-button>
           <el-table
             :data="onlyTableData"
             style="width: 100%">
@@ -109,6 +112,24 @@
       <span slot="footer" class="dialog-footer">
         <el-button @click="editDialogVisible = false">取 消</el-button>
         <el-button type="primary" @click="editParams">确 定</el-button>
+      </span>
+    </el-dialog>
+
+    <!-- 添加参数对话框 -->
+    <el-dialog
+      :title=" '添加' + getTitleText()"
+      :visible.sync="addDialogVisible"
+      width="50%"
+      @close="addDialogClosed"
+    >
+      <el-form :model="addFrom" :rules="addFromRules" ref="addFromRef" label-width="100px">
+        <el-form-item :label="getTitleText()" prop="attr_name">
+          <el-input v-model="addFrom.attr_name"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="addDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addParams">确 定</el-button>
       </span>
     </el-dialog>
   </div>
@@ -300,6 +321,29 @@ export default {
       }
       this.$message.success('删除参数成功！');
       this.getParamsData();
+    },
+    // 监听对话框的关闭事件
+    addDialogClosed() {
+      this.$refs.addFromRef.resetFields();
+    },
+    // 添加参数
+    addParams() {
+      this.$refs.addFromRef.validate(async valid => {
+        if (!valid) return;
+        const { data: res } = await this.$http.post(
+          `categories/${this.getCateId()}/attributes`,
+          {
+            attr_name: this.addFrom.attr_name,
+            attr_sel: this.activeTabsName
+          }
+        );
+        if (res.meta.status !== 201) {
+          return this.$message.error('添加参数失败！');
+        }
+        this.$message.success('添加参数成功！');
+        this.addDialogVisible = false;
+        this.getParamsData();
+      });
     }
   },
   computed: {
