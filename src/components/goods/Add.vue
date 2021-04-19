d6ec87b1868f49aa3678c246ae342ddd87f68ee8
fl6145 <fl_6145@163.com>
2021-04-19 17:23:53 +0800

    渲染基本信息页

--- src/components/goods/Add.vue
+++ src/components/goods/Add.vue
@@ -20,13 +20,42 @@
         <el-step title="完成"></el-step>
       </el-steps>
 
-      <el-tabs tab-position="left" style="height: 200px;" v-model="activeIndex">
-        <el-tab-pane label="基本信息" name="0">基本信息</el-tab-pane>
-        <el-tab-pane label="商品参数" name="1">商品参数</el-tab-pane>
-        <el-tab-pane label="商品属性" name="2">商品属性</el-tab-pane>
-        <el-tab-pane label="商品图片" name="3">商品图片</el-tab-pane>
-        <el-tab-pane label="商品内容" name="4">商品内容</el-tab-pane>
-      </el-tabs>
+      <el-form
+        :model="addForm"
+        :rules="addFormRules"
+        ref="addFormRef"
+        label-width="100px"
+        label-position="top"
+      >
+        <!-- tab 栏-->
+        <el-tabs tab-position="left" v-model="activeIndex">
+          <el-tab-pane label="基本信息" name="0">
+            <el-form-item label="商品名称" prop="goods_name">
+              <el-input v-model="addForm.goods_name"></el-input>
+            </el-form-item>
+            <el-form-item label="商品价格" prop="goods_price">
+              <el-input v-model="addForm.goods_price" type="number"></el-input>
+            </el-form-item>
+            <el-form-item label="商品重量" prop="goods_weight">
+              <el-input v-model="addForm.goods_weight" type="number"></el-input>
+            </el-form-item>
+            <el-form-item label="商品数量" prop="goods_number">
+              <el-input v-model="addForm.goods_number" type="number"></el-input>
+            </el-form-item>
+            <el-form-item label="商品分类" prop="goods_cat">
+              <el-cascader
+                v-model="addForm.goods_cat"
+                :options="cateList"
+                :props="cascaderProps"
+                @change="handleChange"></el-cascader>
+            </el-form-item>
+          </el-tab-pane>
+          <el-tab-pane label="商品参数" name="1">商品参数</el-tab-pane>
+          <el-tab-pane label="商品属性" name="2">商品属性</el-tab-pane>
+          <el-tab-pane label="商品图片" name="3">商品图片</el-tab-pane>
+          <el-tab-pane label="商品内容" name="4">商品内容</el-tab-pane>
+        </el-tabs>
+      </el-form>
     </el-card>
   </div>
 </template>
@@ -37,14 +66,77 @@ import _ from 'lodash';
 export default {
   data() {
     return {
-      activeIndex: 0
+      activeIndex: 0,
+      addForm: {},
+      addFormRules: {
+        goods_name: [
+          {
+            required: true,
+            message: '请输入商品名称',
+            trigger: 'blur'
+          }
+        ],
+        goods_price: [
+          {
+            required: true,
+            message: '请输入商品价格',
+            trigger: 'blur'
+          }
+        ],
+        goods_weight: [
+          {
+            required: true,
+            message: '请输入商品重量',
+            trigger: 'blur'
+          }
+        ],
+        goods_number: [
+          {
+            required: true,
+            message: '请输入商品数量',
+            trigger: 'blur'
+          }
+        ],
+        goods_cat: [
+          {
+            required: true,
+            message: '请选择商品分类',
+            trigger: 'blur'
+          }
+        ]
+      },
+      // 商品列表
+      cateList: [],
+      // 级联选择器配置
+      cascaderProps: {
+        expandTrigger: 'hover',
+        label: 'cat_name',
+        value: 'cat_id',
+        children: 'children'
+      },
+      a: ''
     };
   },
   created() {
-
+    this.getCateList();
   },
   computed: {},
-  methods: {}
+  methods: {
+    // 获取商品分类数据列表
+    async getCateList() {
+      const { data: res } = await this.$http.get('categories');
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取商品列表失败！');
+      }
+      this.cateList = res.data;
+    },
+    handleChange() {
+      if (this.addForm.goods_cat.length !== 3) {
+        this.addForm.goods_cat = [];
+        this.$message.error('只能选择三级的分类');
+      }
+    }
+  }
 };
 </script>
 
