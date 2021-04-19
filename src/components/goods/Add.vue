cd7cdc75c74a7c5ebdaef79407322e5b1d85f2d9
fl6145 <fl_6145@163.com>
2021-04-19 16:54:44 +0800

    tab 和状态栏同步

--- src/components/goods/Add.vue
+++ src/components/goods/Add.vue
@@ -19,6 +19,14 @@
         <el-step title="商品内容"></el-step>
         <el-step title="完成"></el-step>
       </el-steps>
+
+      <el-tabs tab-position="left" style="height: 200px;" v-model="activeIndex">
+        <el-tab-pane label="基本信息" name="0">基本信息</el-tab-pane>
+        <el-tab-pane label="商品参数" name="1">商品参数</el-tab-pane>
+        <el-tab-pane label="商品属性" name="2">商品属性</el-tab-pane>
+        <el-tab-pane label="商品图片" name="3">商品图片</el-tab-pane>
+        <el-tab-pane label="商品内容" name="4">商品内容</el-tab-pane>
+      </el-tabs>
     </el-card>
   </div>
 </template>
@@ -28,7 +36,9 @@ import _ from 'lodash';
 
 export default {
   data() {
-    return {};
+    return {
+      activeIndex: 0
+    };
   },
   created() {
 
