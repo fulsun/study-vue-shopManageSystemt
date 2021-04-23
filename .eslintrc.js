487ac9a0f015b0649869a50dd581143317bacbec
fl_6145 <fl_6145@163.com>
2021-04-23 16:04:29 +0800

    修改vscode在格式化后eslint的语法检查

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,19 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   extends: ['plugin:vue/essential', '@vue/standard'],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    // 不检查分号  ["error", "always"]
-    // 'semi': 'off',
-    // 'quotes': 'off',
-    // 'comma-dangle': 'off',
-    // 'space-before-function-paren': 'off'
-    // 行末分号（启用）
-    'semi': ['warn', 'always'],
-    // 函数名后的空格（禁用）
-    'space-before-function-paren': ['error', 'never'],
-    // 缩进规范（两个空格，一倍缩进）
-    // 'vue/script-indent': ['error', 2, { baseIndent: 1 }],
-    'no-unused-vars': 'off'
-  }
-};
+    'prefer-const': 0, //首选cons
+    'space-before-function-paren': 0,
+  },
+}
 // "no-alert": 0,//禁止使用alert confirm prompt
 //   "no-array-constructor": 2,//禁止使用数组构造器
 //   "no-bitwise": 0,//禁止使用按位运算符
