c8b7a1bce508cc4878a60e254cf4a6b1d5176ae6
fl_6145 <fl_6145@163.com>
2021-04-26 14:47:18 +0800

    去掉生产环境的console babel.config.js文件是可以影响全局的，不仅仅会去掉生产环境的console,还会把开发环境用于调试的console也移除

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,9 @@
+// 这是项目发布阶段需要用到的bebel插件
+const prodPlugins = []
+// 如果是发布阶段,处于生产环境,就向prodPlugins数组里添加一个插件
+if (process.env.NODE_ENV === 'production') {
+  prodPlugins.push('transform-remove-console')
+}
 module.exports = {
   presets: ['@vue/cli-plugin-babel/preset'],
   plugins: [
@@ -7,6 +13,7 @@ module.exports = {
         libraryName: 'element-ui',
         styleLibraryName: 'theme-chalk'
       }
-    ]
+    ],
+    ...prodPlugins // 这里...展开运算符，把上面定义的prodPlugins数组里面的每一项展开添加到这个plugins数组里面
   ]
 }
