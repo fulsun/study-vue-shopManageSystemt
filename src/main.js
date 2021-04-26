a8d33328b83dbcb015030f1431ccce789952e87b
fl_6145 <fl_6145@163.com>
2021-04-26 14:25:59 +0800

    优化：展示进度条

--- src/main.js
+++ src/main.js
@@ -15,6 +15,9 @@ import VueQuillEditor from 'vue-quill-editor'
 import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
+// 导入
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 // Vue.use(ZkTable)
 Vue.component('tree-table', ZkTable)
@@ -26,9 +29,15 @@ Vue.prototype.$http = axios
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 // 请求在到达服务器之前，先会调用use中的这个回调函数来添加请求头信息
 axios.interceptors.request.use((config) => {
+  // NProgress.start()
   // console.log(config)
   // 为请求头对象，添加token验证的Authorization字段
   config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 在最后必须 return config
+  return config
+})
+axios.interceptors.response.use((config) => {
+  // NProgress.done()
   // 在最后必须 return config
   return config
 })
@@ -37,3 +46,13 @@ new Vue({
   router,
   render: (h) => h(App)
 }).$mount('#app')
+
+// 在路由请求之前之后展示进度条
+router.beforeEach((to, from, next) => {
+  NProgress.start()
+  next()
+})
+
+router.afterEach(() => {
+  NProgress.done()
+})
