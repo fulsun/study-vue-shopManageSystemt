2f5fbd3188588ac380743e2dfdc1082a9eb5b28b
fl_6145 <fl_6145@163.com>
2021-04-26 16:18:22 +0800

    优化：使用es语法实现懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,33 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from '../components/Login.vue'
-import Home from '../components/Home.vue'
-import Welcome from '../components/Welcome.vue'
-import Users from '../components/user/Users'
-import Rights from '../components/power/rights'
-import Roles from '../components/power/Roles'
-import Categories from '../components/goods/Categories'
-import Params from '../components/goods/Params'
-import Goods from '../components/goods/List'
-import Add from '../components/goods/Add'
-import Edit from '../components/goods/Edit'
-import Order from '../components/order/Order'
-import Report from '../components/report/Report'
+// import Login from '../components/Login.vue'
+// import Home from '../components/Home.vue'
+// import Welcome from '../components/Welcome.vue'
+// import Users from '../components/user/Users'
+// import Rights from '../components/power/rights'
+// import Roles from '../components/power/Roles'
+// import Categories from '../components/goods/Categories'
+// import Params from '../components/goods/Params'
+// import Goods from '../components/goods/List'
+// import Add from '../components/goods/Add'
+// import Edit from '../components/goods/Edit'
+// import Order from '../components/order/Order'
+// import Report from '../components/report/Report'
+
+// 路由懒加载
+const Login = () => import('@/components/Login.vue')
+const Home = () => import('../components/Home.vue')
+const Welcome = () => import('../components/Welcome.vue')
+const Users = () => import('../components/user/Users')
+const Rights = () => import('../components/power/rights')
+const Roles = () => import('../components/power/Roles')
+const Categories = () => import('../components/goods/Categories')
+const Params = () => import('../components/goods/Params')
+const Goods = () => import('../components/goods/List')
+const Add = () => import('../components/goods/Add')
+const Edit = () => import('../components/goods/Edit')
+const Order = () => import('../components/order/Order')
+const Report = () => import('../components/report/Report')
 
 Vue.use(VueRouter)
 
