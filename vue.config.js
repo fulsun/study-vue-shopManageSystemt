f5a78587fc0b961a2d004ab9833fa4cb582120aa
fl_6145 <fl_6145@163.com>
2021-04-26 15:59:14 +0800

    自定义参数-修改首页：开发环境加载CDN

--- vue.config.js
+++ vue.config.js
@@ -3,18 +3,27 @@ module.exports = {
     config.when(process.env.NODE_ENV === 'production', (config) => {
       config.entry('app').clear().add('./src/main-prod.js')
       config.set('externals', {
-        //包名 ： 全局变量
+        /* 包名 ： 全局变量*/
         vue: 'Vue',
-        'vue-router': 'router',
+        'vue-router': 'VueRouter',
         axios: 'axios',
         lodash: '_',
         echarts: 'echarts',
         nprogress: 'NProgress',
         'vue-quill-editor': 'VueQuillEditor'
       })
+      config.plugin('html').tap((args) => {
+        /* 追加属性 isProd */
+        args[0].isProd = true
+        return args
+      })
     })
     config.when(process.env.NODE_ENV === 'development', (config) => {
       config.entry('app').clear().add('./src/main-dev.js')
+      config.plugin('html').tap((args) => {
+        args[0].isProd = false
+        return args
+      })
     })
   }
 }
